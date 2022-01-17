9298b43140aee40473ac274be257597db5b5959c
Munjongyun <answhddbs413@naver.com>
2022-01-17 11:45:03 +0900

    문종윤 : HomeWork220114 커밋

--- HomeWork/HomeWork220114TextChange/HomeWork220114TextChange.cpp
+++ HomeWork/HomeWork220114TextChange/HomeWork220114TextChange.cpp
@@ -3,20 +3,42 @@
 
 void TextChange(char* _Text, const char* _Prev, const char* _Next)
 {
-	int Index = 0;
-	int Index2 = 0;
+	int PrevIndex = 0;
+	int NextIndex = 0;
+	int TextIndex = 0;
+	bool istrue = false;
 
-	while (_Prev[Index] != 0) // Prev 배열을 돌며 몇 자리 까지 있는지 확인
+	while (_Prev[PrevIndex] != 0) // Prev 배열을 돌며 몇 자리 까지 있는지 확인
 	{
-		Index += 1;
+		PrevIndex += 1;
 	}
 
-	while (_Next[Index2] != 0) // Next 배열을 돌며 몇 자리 까지 있는지 확인
+	while (_Next[NextIndex] != 0) // Next 배열을 돌며 몇 자리 까지 있는지 확인
 	{
-		Index2 += 1;
+		NextIndex += 1;
 	}
 
+	while (_Text[TextIndex] != 0) 
+	{
+		if (_Text[TextIndex] == _Prev[0]) {
+			for (int i = 0; i <= PrevIndex; i++) {
+				if (_Text[TextIndex + i] == _Prev[i]) {
+					istrue = true;
+				}
+				else {
+					istrue = false;
+				}
+			}
+		}
 
+		if (istrue == true) {
+			for (int i = 0; i <= NextIndex; i++) {
+				_Text[TextIndex+i] = _Next[NextIndex+i];
+			}
+		}
+
+		TextIndex += 1;
+	}
 }
 
 
@@ -25,7 +47,7 @@ int main()
 	// 이건 질문 안하셔도 됩니다.
 	char Text[100] = "aa, bb, aa, bb aa aa aa aaaaaaa ccdffeds";
 
-	TextChange(Text, "aa", "fff");
+	TextChange(Text, "aa", "ff");
 	// "fff, bb, fff, bb fff fff fff fffffffffa ccdffeds";
 	printf_s(Text);
 }
