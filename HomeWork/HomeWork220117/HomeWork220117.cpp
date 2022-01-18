1bd4c58c8604fc768097ab1703f2c33a25a5ed14
Munjongyun <answhddbs413@naver.com>
2022-01-18 12:18:21 +0900

    문종윤 : HomeWork220117 업데이트 중

--- HomeWork/HomeWork220117/HomeWork220117.cpp
+++ HomeWork/HomeWork220117/HomeWork220117.cpp
@@ -4,22 +4,25 @@ int CountFirst(const char* _Text, int _Start, const char* _FindStr)
 {
     int FindIndex = 0;
     int TextIndex = 0;
-    int Result = 0;
+    int Result = -1;
 
-    while (_FindStr[FindIndex] != 0) // Prev 배열을 돌며 몇 자리 까지 있는지 확인
+    while (_FindStr[FindIndex] != 0) 
     {
         FindIndex += 1;
     }
 
 	while (_Text[TextIndex] != 0 && TextIndex < _Start)
 	{
-		if (_Text[TextIndex] == _FindStr[0]) { // 만약 Prev의 앞글자와 현재 배열의 글자가 같은 경우
+		if (_Text[TextIndex] == _FindStr[0]) { 
 
-			for (int i = 0; i < FindIndex; i++) {  // Prev 배열과 현재 배열부터 비교해서 다 동일한 경우 istrue를 ture로 만들어줌
+			for (int i = 0; i < FindIndex; i++) {  
 				if (_Text[TextIndex + i] == _FindStr[i]) {
                     Result = TextIndex;
+                }
+                else {
+                    Result = -1;
                     break;
-				}
+                }
 			}
 		}
 
@@ -45,7 +48,9 @@ int main()
 {
     // 실패
     {
-        int Count = CountFirst("aaa eee ttt asdfasd eee", 15, "eee");
+        int Count = CountFirst("aaa  ttt asdfasd", 15, "eee");
+
+        int a = 0;
     }
     /*{
         int Count = CountLast("aaa eee asdfasd eee", 15, "eee");
