9a42ab0bcbda4e098d353619c3f6103764184e2b
Munjongyun <answhddbs413@gmail.com>
2022-02-23 18:36:18 +0900

    문종윤 : HomeWork220223 커밋

---
+++ HomeWork/HomeWork220223/HomeWork220223.cpp
@@ -0,0 +1,22 @@
+﻿// 77Recursivefunction.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int Value = 0;
+
+void Func()
+{
+    std::cout << ++Value << std::endl;
+
+    if (Value >= 30) {
+        return;
+    }
+
+    Func();
+}
+
+int main()
+{
+    Func();
+}
